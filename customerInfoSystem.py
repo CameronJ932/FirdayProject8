4c52b221f0637f1919521316d21aebac292a0746
camjasper1@gmail.com <camjasper1@gmail.com>
2025-04-04 22:05:17 -0500

    Added DataBase setup

--- customerInfoSystem.py
+++ customerInfoSystem.py
@@ -1,6 +1,23 @@
 import sqlite3
 import tkinter as tk
 
+conn = sqlite3.connect('customers.db')
+cursor = conn.cursor()
+
+#Chat GPT creatred 
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS customers(
+    id INTIGER PRIMARY KEY AUTOINCRIMENT,
+    name TEXT NOT NULL,
+    birthday TEXT,
+    email TEXT,
+    phone TEXT,
+    address TEXT,
+    contect_method TEXT,
+    )
+''')
+conn.commit()
+
 def window():
     window = tk.Tk()
     window.title("Info System")
